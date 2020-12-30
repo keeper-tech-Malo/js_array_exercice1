428c19895f2a91d2c6bea422bb21d3e746084504
Malo <ndekezi37@gmail.com>
2020-12-30 14:48:09 +0100

    fin du travail

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+let tableaux = ["apple", "pomme", "banane"];
+console.log(tableaux);
+console.log(tableaux.length);
+
+console.log(tableaux[0],tableaux[1],tableaux[3] ) 
+tableaux.forEach(function(item, index, array) {
+    console.log(item,index)
+})
+
+
+
